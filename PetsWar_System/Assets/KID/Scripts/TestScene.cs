b055ab346efbb70ae31f52d573af38f07364bc59
KID <50666207+KID421@users.noreply.github.com>
2020-09-15 15:22:36 +0800

    隨機場景更新

--- PetsWar_System/Assets/KID/Scripts/TestScene.cs
+++ PetsWar_System/Assets/KID/Scripts/TestScene.cs
@@ -1,14 +1,21 @@
 ﻿using UnityEngine;
 using KID;
-using UnityEngine.SceneManagement;
 
 public class TestScene : MonoBehaviour
 {
+    private void Start()
+    {
+        // 初始化場景名稱
+        RandomScene.SetAllScene("第一關", "第二關", "第三關");
+    }
+
     private void Update()
     {
+        // 在要取得隨機場景的地方
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-            print(RandomScene.LoadRandomScene());
+            // 呼叫 隨機場景.取得隨機場景
+            print(RandomScene.GetRandomScene());
         }
     }
 }
