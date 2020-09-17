d196128a0c0ffcd6cfaee8b5b430d7b4fe1e821f
KID <50666207+KID421@users.noreply.github.com>
2020-09-17 11:31:04 +0800

    可按到節奏：玩家 1

--- PetsWar_System/Assets/KID/Scripts/DanceNode.cs
+++ PetsWar_System/Assets/KID/Scripts/DanceNode.cs
@@ -1,11 +1,43 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class DanceNode : MonoBehaviour
 {
+    /// <summary>
+    /// 節點類型
+    /// </summary>
+    public DancdNodeType node;
+    /// <summary>
+    /// 速度
+    /// </summary>
     public float speed;
+    /// <summary>
+    /// 屬於哪位玩家的節點
+    /// </summary>
+    public int indexPlayer;
+
+    private bool outOfLine;
+
+    private DanceManager dm;
+
+    private void Awake()
+    {
+        dm = FindObjectOfType<DanceManager>();
+    }
 
     private void Update()
     {
         transform.Translate(-speed * Time.deltaTime, 0, 0);
+
+        if (GetComponent<RectTransform>().anchoredPosition.x <= -350 && !outOfLine)
+        {
+            outOfLine = true;
+            dm.nodePlaysIndex[indexPlayer]++;
+        }
+        if (outOfLine && GetComponent<RectTransform>().anchoredPosition.x <= -380)
+        {
+            List<DancdNodeType> nodes = dm.nodesPlays[indexPlayer];
+            nodes[dm.nodePlaysIndex[indexPlayer] - 1] = DancdNodeType.無;
+        }
     }
 }
