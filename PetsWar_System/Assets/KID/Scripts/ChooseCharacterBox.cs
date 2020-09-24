376a2b9669949c9aa4df2aa1d8ab69db29c0e3aa
KID <50666207+KID421@users.noreply.github.com>
2020-09-24 17:48:43 +0800

    新版選取角色

---
+++ PetsWar_System/Assets/KID/Scripts/ChooseCharacterBox.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+namespace KID
+{
+    public class ChooseCharacterBox : MonoBehaviour
+    {
+        [Header("玩家資料：玩家 1 - 玩家 4")]
+        /// <summary>
+        /// 玩家資料：玩家 1 - 玩家 4
+        /// </summary>
+        public PlayerData[] players;
+        [Header("選取角色盒子")]
+        public Image[] imgBoxes;
+
+        /// <summary>
+        /// 選取角色方塊的四個座標
+        /// </summary>
+        public List<float> posCharacters = new List<float> { -447, -149, 149, 447 };
+
+        public List<int> indexCharacters = new List<int> { 0, 1, 2, 3 };
+
+        private void Update()
+        {
+            MoveBox();
+        }
+
+        /// <summary>
+        /// 移動選取角色盒子
+        /// </summary>
+        private void MoveBox()
+        {
+            if (Input.anyKeyDown)
+            {
+                for (int i = 0; i < players.Length; i++)
+                {
+                    if (Input.GetKeyDown(players[i].right))
+                    {
+                        int index = indexCharacters[i];
+                        index++;
+
+                        if (index >= posCharacters.Count) index = 0;
+
+                        indexCharacters[i] = index;
+
+                        imgBoxes[i].rectTransform.anchoredPosition = new Vector2(posCharacters[index], 0);
+                    }
+
+                    if (Input.GetKeyDown(players[i].left))
+                    {
+                        int index = indexCharacters[i];
+                        index--;
+
+                        if (index <= -1) index = posCharacters.Count - 1;
+
+                        indexCharacters[i] = index;
+
+                        imgBoxes[i].rectTransform.anchoredPosition = new Vector2(posCharacters[index], 0);
+                    }
+                }
+            }
+        }
+    }
+}
+
